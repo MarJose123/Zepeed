baf4db136cdc6e7a375445141a13d852cbb50bae
MarJose Darang <18107626+MarJose123@users.noreply.github.com>
2026-06-19 09:40:34 +0800

    refactor(auth): Remove unused login features (#34)

--- config/fortify.php
+++ config/fortify.php
@@ -145,9 +145,6 @@ return [
     */
 
     'features' => [
-        Features::registration(),
-        Features::resetPasswords(),
-        Features::emailVerification(),
         Features::twoFactorAuthentication([
             'confirm'         => true,
             'confirmPassword' => true,
